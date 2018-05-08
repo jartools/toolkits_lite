121c5e4c49fe92deb1593d6ce3fbad9353504732
canyon <gongyanghui1986@163.com>
2018-05-08 15:29:17 +0800

    修改继承对象，为TkitJsp

--- src/com/bowlong/Toolkit.java
+++ src/com/bowlong/Toolkit.java
@@ -5,10 +5,10 @@ import java.util.Map;
 import redis.clients.jedis.JedisPool;
 
 import com.bowlong.third.redis.JedisTookits;
-import com.bowlong.tool.TkitBase;
+import com.bowlong.tool.TkitJsp;
 
 @SuppressWarnings({ "rawtypes", "unchecked" })
-public class Toolkit extends TkitBase {
+public class Toolkit extends TkitJsp {
 
 	static public Map<String, Object> RedisMap() {
 		return JedisTookits.getOrgRedisCfg();
