949b7e8fc1679b0abccb7990f4130a964a45d61d
canyons <gongyanghui1986@163.com>
2018-11-10 21:43:16 +0800

    添加过滤和初始化方法

--- src/com/bowlong/third/jsp/BasicFilter.java
+++ src/com/bowlong/third/jsp/BasicFilter.java
@@ -19,8 +19,9 @@ import com.bowlong.tool.TkitJsp;
 import com.bowlong.util.CalendarEx;
 import com.bowlong.util.MapEx;
 /***
- * jsp filter基础过滤文件
- * 可以设置编码
+ * jsp filter基础过滤文件 <br/>
+ * 启动的顺序为listener->Filter->servlet.<br/>
+ * 简单记为：理(Listener)发(Filter)师(servlet).
  * @author Canyon 2017-04-16 23:30
  */
 public abstract class BasicFilter implements Filter {
@@ -35,6 +36,8 @@ public abstract class BasicFilter implements Filter {
 	static protected long ms_aft = CalendarEx.TIME_SECOND * 15;
 	static protected String key_time = "time_ms";
 	static public boolean isValidSql = true;
+	static public boolean isPrint = false;
+	static private boolean isInit = false;
 
 	// 效验
 	static private boolean sqlValidate(String str) {
@@ -61,17 +64,19 @@ public abstract class BasicFilter implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 		req.setCharacterEncoding(strEncoding);
 		HttpServletResponse res = (HttpServletResponse) response;
-		Map<String, String> valMap = TkitJsp.getMapAllParams(req);
+		Map<String, String> pars = TkitJsp.getMapAllParams(req,true);
 		String val = req.getRequestURI();
+		
+		_print(val, pars);
 
-		boolean isFlag = isFilter(val, valMap) || isFilterSql(valMap)
-				|| isFilterTime(valMap, key_time);
+		boolean isFlag = isFilter(val, pars) || isFilterSql(pars)
+				|| isFilterTime(pars, key_time);
 
 		if (isFlag) {
-			valMap.clear();
-			valMap.put("code", "fails");
-			valMap.put("cmd", val);
-			JSONObject jsonData = JsonHelper.toJSON(valMap);
+			pars.clear();
+			pars.put("code", "fails");
+			pars.put("cmd", val);
+			JSONObject jsonData = JsonHelper.toJSON(pars);
 			TkitJsp.writeAndClose(res, jsonData.toString(), "UTF-8");
 			return;
 		}
@@ -86,20 +91,25 @@ public abstract class BasicFilter implements Filter {
 			encoding = "UTF-8";
 		}
 		this.strEncoding = encoding.trim();
+		
+		if(!isInit){
+			isInit = true;
+			onInit(arg0);
+		}
 	}
 
-	protected boolean isFilterTime(Map<String, String> map, String keyTime) {
-		if (map.containsKey(keyTime)) {
-			long time_ms = MapEx.getLong(map, keyTime);
+	protected boolean isFilterTime(Map<String, String> pars, String keyTime) {
+		if (pars.containsKey(keyTime)) {
+			long time_ms = MapEx.getLong(pars, keyTime);
 			long curr_ms = CalendarEx.now();
 			return !(time_ms > curr_ms - ms_bef && time_ms < curr_ms + ms_aft);
 		}
 		return false;
 	}
 
-	protected boolean isFilterSql(Map<String, String> map) {
+	protected boolean isFilterSql(Map<String, String> pars) {
 		boolean isFlag = false;
-		Object[] _arrs = map.values().toArray();
+		Object[] _arrs = pars.values().toArray();
 		int lens = _arrs.length;
 		for (int i = 0; i < lens; i++) {
 			if (isFlag)
@@ -109,6 +119,14 @@ public abstract class BasicFilter implements Filter {
 		}
 		return isFlag;
 	}
+	
+	private void _print(String uri, Map<String, String> pars){
+		if(!isPrint)
+			return;
+		JSONObject jsonData = JsonHelper.toJSON(pars);
+		System.out.println(String.format("%s = %s", uri,jsonData.toString()));
+	}
 
-	public abstract boolean isFilter(String uri, Map<String, String> map);
+	public abstract boolean isFilter(String uri, Map<String, String> pars);
+	public abstract void onInit(FilterConfig arg0);
 }
