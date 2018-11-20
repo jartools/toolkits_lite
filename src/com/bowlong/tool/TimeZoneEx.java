378f86be489d1822b350129191a62c69f2d7b592
canyon <gongyanghui1986@163.com>
2018-11-20 18:17:09 +0800

    取得时区的int值

--- src/com/bowlong/tool/TimeZoneEx.java
+++ src/com/bowlong/tool/TimeZoneEx.java
@@ -1,5 +1,6 @@
 package com.bowlong.tool;
 
+import java.util.Calendar;
 import java.util.TimeZone;
 
 import com.bowlong.lang.StrEx;
@@ -15,20 +16,31 @@ public class TimeZoneEx {
 	static public final String getJVMTimeZoneID() {
 		return System.getProperty("user.timezone");
 	}
-	
+
 	static public final String setJVMTimeZoneID(String Id) {
-		if(StrEx.isEmptyTrim(Id)){
+		if (StrEx.isEmptyTrim(Id)) {
 			// 8区(中国)
 			Id = "GMT+08";
 		}
-		return System.setProperty("user.timezone",Id);
+		return System.setProperty("user.timezone", Id);
 	}
-	
+
 	static public final TimeZone getJVMTimeZone() {
 		return TimeZone.getTimeZone(getJVMTimeZoneID());
 	}
-	
+
 	static public final TimeZone getDefaultTimeZone() {
 		return TimeZone.getDefault();
+	}
+	
+	/** 服务器时区值-小时 */
+	static public final int getTZHour() {
+		Calendar cal = Calendar.getInstance();
+		int offset = cal.get(Calendar.ZONE_OFFSET);
+		cal.add(Calendar.MILLISECOND, -offset);
+		Long timeStampUTC = cal.getTimeInMillis();
+		Long timeStamp = System.currentTimeMillis();
+		Long timeZone = (timeStamp - timeStampUTC) / (1000 * 3600);
+		return timeZone.intValue();
 	}
 }
