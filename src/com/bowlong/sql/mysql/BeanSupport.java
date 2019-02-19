cf337f1ee2f8349d855743547199e5e09309696a
canyons <gongyanghui1986@163.com>
2019-02-19 22:27:54 +0800

    修改警告

--- src/com/bowlong/sql/mysql/BeanSupport.java
+++ src/com/bowlong/sql/mysql/BeanSupport.java
@@ -1,7 +1,6 @@
 package com.bowlong.sql.mysql;
 
+@SuppressWarnings("unchecked")
 public abstract class BeanSupport extends com.bowlong.sql.beanbasic.BeanSupport {
-	
 	private static final long serialVersionUID = 1L;
-	
 }
