243243485f7869eaa49432d9590bcc2b5cfe23e3
canyon <gongyanghui1986@163.com>
2019-05-27 10:22:40 +0800

    修改正则匹配脚本

--- src/com/bowlong/regex/RegxEx.java
+++ src/com/bowlong/regex/RegxEx.java
@@ -5,184 +5,107 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@SuppressWarnings({ "unchecked", "rawtypes" })
+/**
+ * 正则匹配
+ * @author Canyon
+ *
+ */
 public class RegxEx {
+	// 是否有匹配
+	static final public boolean isMatch(String str, String regx) {
+		Pattern p = Pattern.compile(regx);
+		Matcher m = p.matcher(str);
+		return m.matches();
+	}
+
 	// 查找以指定字符开始，并以指定字符结尾的数据集
-	public static List MatchBeginAndEnd(String str, String begin, String end) {
-		Pattern p = null;
-		Matcher m = null;
-		List ret = new ArrayList();
-		int start = 0;
-
+	static final public List<String> matchBeginAndEnd(String str, String begin, String end) {
 		String regx = begin + "[\\s\\S]*?" + end;
-
-		p = Pattern.compile(regx);
-		m = p.matcher(str);
+		List<String> ret = new ArrayList<String>();
+		int start = 0;
+		Pattern p = Pattern.compile(regx);
+		Matcher m = p.matcher(str);
 		while (m.find(start)) {
 			String item = m.group();
 			start = m.end();
 			ret.add(item);
 		}
-
-		m = null;
-		p = null;
 		return ret;
 	}
 
 	// 查找以指定字符开始，并以指定字符结尾的数据集，并且包含指定字符串
-	public static List MatchBeginAndEndAndInc(String str, String begin,
-			String end, String inc) {
-		Pattern p = null;
-		Matcher m = null;
-		List ret = new ArrayList();
-		int start = 0;
-
+	static final public List<String> matchBeginAndEndAndInc(String str, String begin, String end, String inc) {
 		String regx = begin + "[\\s\\S]*" + inc + "[\\s\\S]*?" + end;
-		p = Pattern.compile(regx);
-		m = p.matcher(str);
+		List<String> ret = new ArrayList<String>();
+		int start = 0;
+		Pattern p = Pattern.compile(regx);
+		Matcher m = p.matcher(str);
 		while (m.find(start)) {
 			String item = m.group();
 			start = m.end();
 			ret.add(item);
 		}
-
-		p = null;
-		m = null;
-		return ret;
-	}
-
-	// ????是否只有数字
-	public static boolean MatchNum(String str) {
-		Pattern p = null;
-		Matcher m = null;
-		boolean ret = false;
-
-		String regx = "^[0-9]*$";
-		p = Pattern.compile(regx);
-		m = p.matcher(str);
-		ret = m.matches();
-
-		p = null;
-		m = null;
 		return ret;
 	}
 
 	// 只能输入n位的数字
-	public static boolean MatchNBitNum(String str, int n) {
-		Pattern p = null;
-		Matcher m = null;
-		boolean ret = false;
+	static final public boolean matchNum(String str, int nbegn, int nend) {
+		String regx = null;
+		if (nbegn >= 0) {
+			if (nend > nbegn)
+				regx = "^\\d{" + nbegn + "," + nend + "}$";
+			else if (nend == 0)
+				regx = "^\\d{" + nbegn + ",}$";
+			else
+				regx = "^\\d{" + nbegn + "}$";
+		} else {
+			regx = "^[0-9]*$";
+		}
+		return isMatch(str, regx);
+	}
 
-		String regx = "^\\d{" + n + "}$";
-		p = Pattern.compile(regx);
-		m = p.matcher(str);
-		ret = m.matches();
-
-		p = null;
-		m = null;
-		return ret;
+	// ????是否只有数字
+	static final public boolean matchNum(String str) {
+		return matchNum(str, -1, 0);
 	}
 
 	// 只能输入至少n位的数字
-	public static boolean MatchThanNBitNum(String str, int n) {
-		Pattern p = null;
-		Matcher m = null;
-		boolean ret = false;
-
-		String regx = "^\\d{" + n + ",}$";
-		p = Pattern.compile(regx);
-		m = p.matcher(str);
-		ret = m.matches();
-
-		p = null;
-		m = null;
-		return ret;
+	static final public boolean matchNumThan(String str, int n) {
+		return matchNum(str, n, 0);
 	}
 
 	// 只能输入m~n位的数字
-	public static boolean MatchBetweenTwoNum(String str, int n1, int n2) {
-		Pattern p = null;
-		Matcher m = null;
-		boolean ret = false;
-
-		String regx = "^\\d{" + n1 + "," + n2 + "}$";
-		p = Pattern.compile(regx);
-		m = p.matcher(str);
-		ret = m.matches();
-
-		p = null;
-		m = null;
-		return ret;
+	static final public boolean matchNumBetween(String str, int n1, int n2) {
+		return matchNum(str, n1, n2);
 	}
 
 	// 只能输入零和非零????的数??
-	public static boolean MatchNumBeginStr(String str) {
-		Pattern p = null;
-		Matcher m = null;
-		boolean ret = false;
-
-		String regx = "^(0|[1-9][\\s\\S]*)$";
-		p = Pattern.compile(regx);
-		m = p.matcher(str);
-		ret = m.matches();
-
-		p = null;
-		m = null;
-		return ret;
+	static final public boolean matchNumBeginStr(String str) {
+		return isMatch(str, "^(0|[1-9][\\s\\S]*)$");
 	}
-	
-	public static List href(String str) {
-		Pattern p = null;
-		Matcher m = null;
-		List ret = new ArrayList();
-		int start = 0;
 
+	static final public List<String> href(String str) {
 		String regx = "href\\s*=\\s*(?:\"([^\"]*)\"|'([^']*)'|([^\"'>\\s]+))";
-
-		p = Pattern.compile(regx);
-		m = p.matcher(str);
+		List<String> ret = new ArrayList<String>();
+		int start = 0;
+		Pattern p = Pattern.compile(regx);
+		Matcher m = p.matcher(str);
 		while (m.find(start)) {
 			String item = m.group();
 			start = m.end();
 			ret.add(item);
 		}
-		p = null;
-		m = null;
 		return ret;
 	}
 
-	public static String HtmlToText(String html) {
-		Pattern p = null;
-		Matcher m = null;
-		String ret = "";
-
+	static final public String htmlToText(String html) {
 		String regx = "<[^>]+>";
-		p = Pattern.compile(regx);
-		m = p.matcher(html);
-		ret = m.replaceAll("");
-
-		p = null;
-		m = null;
-		return ret;
-
+		Pattern p = Pattern.compile(regx);
+		Matcher m = p.matcher(html);
+		return m.replaceAll("");
 	}
 
-	public static boolean regexAnsi(String GID) {
-		boolean result = false;
-		String reg = null;
-		Pattern p = null;
-		Matcher m = null;
-		try {
-			reg = "^\\w+$";
-			p = Pattern.compile(reg);
-			m = p.matcher(GID);
-			result = m.matches();
-		} catch (Exception e) {
-		} finally {
-			reg = null;
-			p = null;
-			m = null;
-		}
-		return result;
+	static final public boolean regexAnsi(String GID) {
+		return isMatch(GID, "^\\w+$");
 	}
 }
