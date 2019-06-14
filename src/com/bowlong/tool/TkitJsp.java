a8f4cffdfca9ad1087a996c8c10811bfec400529
canyon <gongyanghui1986@163.com>
2019-06-14 17:30:54 +0800

    添加ip对应code http://software77.net/geo-ip/

--- src/com/bowlong/tool/TkitJsp.java
+++ src/com/bowlong/tool/TkitJsp.java
@@ -22,15 +22,12 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.alibaba.fastjson.JSON;
-import com.bowlong.lang.StrEx;
 import com.bowlong.net.http.HttpBaseEx;
 import com.bowlong.objpool.StringBufPool;
 import com.bowlong.third.xml.province.entity.XmlCities;
 import com.bowlong.third.xml.province.entity.XmlCity;
 import com.bowlong.third.xml.province.entity.XmlProvinces;
-import com.bowlong.util.DateEx;
 import com.bowlong.util.ListEx;
-import com.bowlong.util.MapEx;
 
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class TkitJsp extends TkitOrigin {
@@ -43,7 +40,7 @@ public class TkitJsp extends TkitOrigin {
 		for (Object key : orign.keySet()) {
 			Object val = orign.get(key);
 			if (val instanceof Date) {
-				val = format((Date) val, DateEx.fmt_yyyy_MM_dd_HH_mm_ss);
+				val = format((Date) val, fmt_yyyy_MM_dd_HH_mm_ss);
 			}
 			result.put(key, val);
 		}
@@ -51,8 +48,8 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/*** 添加消息 **/
-	static public final Map tipMap(Map map, int result, String msg) {
-		if (MapEx.isEmpty(map))
+	static final public Map tipMap(Map map, int result, String msg) {
+		if (isEmpty(map))
 			map = new HashMap();
 		map.put("msg", msg);
 		map.put("status", result);
@@ -60,7 +57,7 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/*** 输出文本内容 **/
-	static public final void writeAndClose(OutputStream out, String content, String charset) {
+	static final public void writeAndClose(OutputStream out, String content, String charset) {
 		try {
 			charset = HttpBaseEx.reCharset(charset);
 			out.write(content.getBytes(charset));
@@ -72,7 +69,7 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/*** 输出文本内容 **/
-	static public final void writeAndClose(HttpServletResponse res, String content, String charset) {
+	static final public void writeAndClose(HttpServletResponse res, String content, String charset) {
 		try {
 			charset = HttpBaseEx.reCharset(charset);
 
@@ -84,14 +81,14 @@ public class TkitJsp extends TkitOrigin {
 		}
 	}
 
-	static public final void writeJsonAndClose(OutputStream out, String strJson, String callBackFun) {
-		if (!StrEx.isEmptyTrim(callBackFun)) {
+	static final public void writeJsonAndClose(OutputStream out, String strJson, String callBackFun) {
+		if (!isEmptyTrim(callBackFun)) {
 			strJson = callBackFun + "(" + strJson + ")";
 		}
 		writeAndClose(out, strJson, "");
 	}
 
-	static public final void writeJsonAndClose(HttpServletResponse response, String strJson, String callBackFun) {
+	static final public void writeJsonAndClose(HttpServletResponse response, String strJson, String callBackFun) {
 		try {
 			ServletOutputStream out = response.getOutputStream();
 			writeJsonAndClose(out, strJson, callBackFun);
@@ -101,7 +98,7 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/*** json格式写出map对象 [callfun:为空，就默认方式,不为空，就callFun方式] **/
-	static public final void writeAndClose(OutputStream out, Map map, String callBackFun) {
+	static final public void writeAndClose(OutputStream out, Map map, String callBackFun) {
 		try {
 			String v = JSON.toJSONString(map);
 			writeJsonAndClose(out, v, callBackFun);
@@ -111,7 +108,7 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/*** json格式写出map对象 **/
-	static public final void writeAndClose(HttpServletResponse response, Map map, String callBackFun) {
+	static final public void writeAndClose(HttpServletResponse response, Map map, String callBackFun) {
 		try {
 			ServletOutputStream out = response.getOutputStream();
 			writeAndClose(out, map, callBackFun);
@@ -121,22 +118,22 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/*** json格式写出map对象 **/
-	static public final void writeAndClose(OutputStream out, Map map) {
+	static final public void writeAndClose(OutputStream out, Map map) {
 		writeAndClose(out, map, null);
 	}
 
 	/*** json格式写出map对象 **/
-	static public final void writeAndClose(HttpServletResponse response, Map map) {
+	static final public void writeAndClose(HttpServletResponse response, Map map) {
 		writeAndClose(response, map, null);
 	}
 
 	/*** 清空httpsession **/
-	static public final void clearHttpSession(HttpSession session) {
+	static final public void clearHttpSession(HttpSession session) {
 		clearHttpSession(session, "");
 	}
 
 	/*** 清空httpsession **/
-	static public final void clearHttpSession(HttpSession session, String... excepts) {
+	static final public void clearHttpSession(HttpSession session, String... excepts) {
 		if (session == null)
 			return;
 		List<String> list = ListEx.toList(excepts);
@@ -150,9 +147,8 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/*** 添加Cookie的实现 **/
-	static public final void addCookie(String name, String password, HttpServletResponse response,
-			HttpServletRequest request) throws Exception {
-		if (!StrEx.isEmptyTrim(name) && !StrEx.isEmptyTrim(password)) {
+	static final public void addCookie(String name, String password, HttpServletResponse response, HttpServletRequest request) throws Exception {
+		if (!isEmptyTrim(name) && !isEmptyTrim(password)) {
 			// 创建Cookie
 			Cookie nameCookie = new Cookie("name", URLEncoder.encode(name, "utf-8"));
 			Cookie pswCookie = new Cookie("psw", password);
@@ -179,7 +175,7 @@ public class TkitJsp extends TkitOrigin {
 		}
 	}
 
-	static public final void getCookie(HttpServletRequest request) throws Exception {
+	static final public void getCookie(HttpServletRequest request) throws Exception {
 		// <%
 		String name = "";
 		String psw = "";
@@ -205,8 +201,7 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/*** 上传图片demo **/
-	static public void upload(HttpServletRequest request, MultipartHttpServletRequest fileRequest,
-			HttpServletResponse response) {
+	static final public void upload(HttpServletRequest request, MultipartHttpServletRequest fileRequest, HttpServletResponse response) {
 		String pathDir = "";
 		/** 得到图片保存目录的真实路径 **/
 		String logoRealPathDir = request.getSession().getServletContext().getRealPath(pathDir);
@@ -218,7 +213,7 @@ public class TkitJsp extends TkitOrigin {
 		MultipartFile multipartFile = fileRequest.getFile("img2Upload");
 		/** 获取文件的后缀 **/
 		System.out.println(multipartFile.getOriginalFilename());
-		String suffix = suffix(multipartFile.getOriginalFilename(),true);
+		String suffix = suffix(multipartFile.getOriginalFilename(), true);
 
 		/** 拼成完整的文件保存路径加文件 **/
 		String name = +System.currentTimeMillis() + suffix;
@@ -234,27 +229,27 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/*** 取得省份 **/
-	static public final void provinces(String path, ModelMap modelMap) {
+	static final public void provinces(String path, ModelMap modelMap) {
 		if (path == null)
 			path = "";
 		XmlProvinces provinces = XmlProvinces.getEn4JarCache(path);
-		if (provinces != null && !ListEx.isEmpty(provinces.getList())) {
+		if (provinces != null && !isEmpty(provinces.getList())) {
 			modelMap.put("provinces", provinces.getList());
 		}
 	}
 
 	/*** 根据省ID，取得该省份拥有的市区 **/
-	static public final void getCitiesByPid(String path, int pid, ModelMap modelMap) {
+	static final public void getCitiesByPid(String path, int pid, ModelMap modelMap) {
 		if (path == null)
 			path = "";
 		List<XmlCity> list = XmlCities.getList4Jar(path, pid);
-		if (!ListEx.isEmpty(list)) {
+		if (!isEmpty(list)) {
 			modelMap.put("cities", list);
 		}
 	}
 
 	static final boolean isNullUnknownIP(String ip) {
-		if (StrEx.isEmptyTrim(ip))
+		if (isEmptyTrim(ip))
 			return true;
 		return "unknown".equalsIgnoreCase(ip);
 	}
@@ -265,7 +260,7 @@ public class TkitJsp extends TkitOrigin {
 	 * 
 	 * @return 客户端真实IP地址,如:202.65.16.220
 	 */
-	static public final String getVisitorIP(HttpServletRequest request) {
+	static final public String getVisitorIP(HttpServletRequest request) {
 		String ip = request.getHeader("x-forwarded-for");
 		if (isNullUnknownIP(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
@@ -280,12 +275,29 @@ public class TkitJsp extends TkitOrigin {
 	}
 
 	/**
+	 * 取得ip4对应的城市code-Long值
+	 * @param visIP 255.255.255.0
+	 * @return
+	 */
+	static final public String ip4CountryCode(String visIP) {
+		List<Integer> list = ListEx.toListInt(visIP);
+		long val = 0L;
+		int lens = list.size();
+		int counter = 3;
+		for (int i = 0; i < lens; i++) {
+			val += list.get(i) * (Math.pow(256, counter));
+			counter--;
+		}
+		return toStr(val);
+	}
+
+	/**
 	 * 获取项目的IP地址<br>
 	 * 
 	 * @return 协议://服务器名称:Web应用的端口号 <br/>
 	 *         如：http://127.0.0.1:81
 	 */
-	static public final String getUrlIP(HttpServletRequest request) {
+	static final public String getUrlIP(HttpServletRequest request) {
 		StringBuffer buff = StringBufPool.borrowObject();
 		try {
 			// 取得协议，如：http
@@ -310,7 +322,7 @@ public class TkitJsp extends TkitOrigin {
 	 * @return 协议://服务器名称:Web应用的端口号/Context路径 <br/>
 	 *         如：http://127.0.0.1:81/项目名称
 	 */
-	static public final String getUrlIPProject(HttpServletRequest request) {
+	static final public String getUrlIPProject(HttpServletRequest request) {
 		return getUrlIP(request).concat(request.getContextPath());
 	}
 
@@ -320,13 +332,13 @@ public class TkitJsp extends TkitOrigin {
 	 * @return 协议://服务器名称:Web应用的端口号/uri <br/>
 	 *         如：http://127.0.0.1:81/项目名称/函数
 	 */
-	static public final String getUriAll(HttpServletRequest request) {
+	static final public String getUriAll(HttpServletRequest request) {
 		return getUrlIP(request).concat(request.getRequestURI());
 	}
 
 	static String _dirTmWebRoot = null;
 
-	static public final String getDirTmWebRoot() {
+	static final public String getDirTmWebRoot() {
 		if (null == _dirTmWebRoot) {
 			_dirTmWebRoot = dirTmWebRoot();
 		}
