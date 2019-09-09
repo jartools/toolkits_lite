e1cead4e159b93443acf02f5d550b03df6431da2
canyon <gongyanghui1986@163.com>
2019-09-09 10:20:06 +0800

    完善Object对象的pool

--- src/com/bowlong/objpool/ObjPool.java
+++ src/com/bowlong/objpool/ObjPool.java
@@ -1,15 +1,25 @@
 package com.bowlong.objpool;
 
-@SuppressWarnings({"unchecked" })
+/***
+ * 所有Class对象的Pool
+ * @author Canyon/龚阳辉
+ * 2019-09-09 10:19
+ */
+@SuppressWarnings({ "unchecked" })
 public class ObjPool {
+
+	static final public <T> BasicPool<T> getPool(Class<T> c) {
+		BasicPool<T> pool = BasicPool.getPool(c);
+		if (pool == null) {
+			pool = new BasicPool<T>(c);
+			BasicPool.initPool(pool);
+		}
+		return pool;
+	}
+
 	static final public <T> T borrowObject(Class<T> c) {
 		try {
-			BasicPool<T> pool = BasicPool.getPool(c);
-			if (pool == null) {
-				pool = new BasicPool<T>(c);
-				BasicPool.initPool(pool);
-			}
-			return (T) pool.borrow();
+			return (T) getPool(c).borrow();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -21,12 +31,7 @@ public class ObjPool {
 			return;
 		try {
 			Class<T> c = (Class<T>) obj.getClass();
-			BasicPool<T> pool = BasicPool.getPool(c);
-			if (pool == null) {
-				pool = new BasicPool<T>(c);
-				BasicPool.initPool(pool);
-			}
-			pool.returnObj(obj);
+			getPool(c).returnObj(obj);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
