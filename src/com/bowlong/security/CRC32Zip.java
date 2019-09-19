bd9410dbfde600dc71aa8144fdcd43c66e017587
canyon <gongyanghui1986@163.com>
2019-09-19 20:14:58 +0800

    添加string对象的crc32算法

--- src/com/bowlong/security/CRC32Zip.java
+++ src/com/bowlong/security/CRC32Zip.java
@@ -2,14 +2,17 @@ package com.bowlong.security;
 
 import java.util.zip.CRC32;
 
+import com.bowlong.basic.ExToolkit;
+
 /**
  * CRC32 Zip算法
  * 
  * @author Canyon
  * @version 2019-03-28 18:50
  */
-public class CRC32Zip {
+public class CRC32Zip extends ExToolkit {
 	static final CRC32 _obj = new CRC32();
+
 	static final public long crc32(byte[] buff) {
 		_obj.reset();
 		_obj.update(buff);
@@ -17,7 +20,18 @@ public class CRC32Zip {
 	}
 
 	static final public String crc32Hex(byte[] buff) {
-		long _v = crc32(buff);
-		return Long.toHexString(_v);
+		return Long.toHexString(crc32(buff));
+	}
+
+	static final public long crc32(String val) {
+		if (isEmpty(val))
+			return 0L;
+		return crc32(val.getBytes());
+	}
+
+	static final public String crc32Hex(String val) {
+		if (isEmpty(val))
+			return "";
+		return crc32Hex(val.getBytes());
 	}
 }
